755399e2b5c4bc5767a7028389d5b8f1125cd777
Utkarsh Singh <utkarshsinghcs@gmail.com>
2024-04-06 10:58:16 +0530

    Agency Collab and website builder project

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactNode } from 'react';
+
+const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return <div className="h-full flex items-center justify-center">{children}</div>;
+};
+
+export default AuthLayout;
